7eaaf3ccf33a234680019cd2e0e2856a8c3b8f6f
Aarav <73211719+aarav241@users.noreply.github.com>
2021-02-17 10:38:41 +0530

    Add files via upload

--- Paper(1).js
+++ Paper(1).js
@@ -1,7 +1,6 @@
-class Paper{
-constructor(x,y,r){
-var option={
-isStatic:false,
+class Paper2{
+    constructor(x,y,r){
+    var option={
 'restitution':0.3,
 'friction':0.5,
 'density':1.2
@@ -11,7 +10,9 @@ this.x =x;
 this.y =y;
 this.r = r;
 
-this.Body = Bodies.circle(this.x,this.y,this.r/2,option) 
+this.Body = Bodies.circle(this.x,this.y,this.r/2,option) ;
+this.image = loadImage("paper.png");
+//Matter.Body.Setscale(this.Body,0.1)
 World.add(world,this.Body)
 }
 display(){
@@ -19,11 +20,12 @@ display(){
     var pos = this.Body.position;
 push()
 translate(pos.x,pos.y);
-rectMode(CENTER);
-strokeWeight(3);
-fill(255,0,255);
-ellipse(0,0,this.r,this.r);
-pop()
+imageMode(CENTER);
+//strokeWeight(3);
+//fill(255,0,255);
+//ellipse(0,0,this.r,this.r)
+image(this.image,0,0,100,100);
+pop();
 }
 
 
